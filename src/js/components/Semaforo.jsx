022314c01fa5b8d99b359938eacf4ed701761316
Jhon Cordova <jhon.a.cordova.martinez@gmail.com>
2025-07-12 00:49:49 +0000

    12jul25

---
+++ src/js/components/Semaforo.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import '/workspaces/JCM96-Semaforo/src/styles/index.css'; // Importamos nuestros nuevos estilos
+
+const Semaforo = () => {
+    // 1 estado para saber qué luz está activa.
+    // 'red' por defecto.
+    const [luzActiva, setLuzActiva] = useState('red');
+
+    return (
+        <div id="Semaforo">
+            <div className="semaforo-container">
+                {/* Luz Roja */}
+                <div
+                    // 2. Aplicamos clases dinámicamente
+                    className={`luz roja ${luzActiva === 'red' ? 'glow' : ''}`}
+                    // 3. Al hacer clic, cambiamos el estado a 'red'
+                    onClick={() => setLuzActiva('red')}
+                >
+                </div>
+
+                {/* Luz Amarilla */}
+                <div
+                    className={`luz amarilla ${luzActiva === 'yellow' ? 'glow' : ''}`}
+                    onClick={() => setLuzActiva('yellow')}
+                >
+                </div>
+
+                {/* Luz Verde */}
+                <div
+                    className={`luz verde ${luzActiva === 'green' ? 'glow' : ''}`}
+                    onClick={() => setLuzActiva('green')}
+                >
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Semaforo;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -10,9 +10,12 @@ import '../styles/index.css'
 
 // components
 import Home from './components/Home';
+import Semaforo from './components/Semaforo';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    {/* dejar home para futuras referencias */}
+    {/* <Home/> */}
+    <Semaforo/>
   </React.StrictMode>,
 )
